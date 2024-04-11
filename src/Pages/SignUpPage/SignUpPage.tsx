bb65b611e97afd1ef3bf692acabc15829594e31a
Jamie Pacheco <jamie.pacheco.71@gmail.com>
2024-04-11 14:04:48 -0400

    added input elements to sign up page

--- src/Pages/SignUpPage/SignUpPage.tsx
+++ src/Pages/SignUpPage/SignUpPage.tsx
@@ -1,25 +1,45 @@
-import { Form} from "react-bootstrap"
+import { Col, Container, Form, Row} from "react-bootstrap"
 import "./SignUpPage.css"
 
 export function SignUpPage() : React.JSX.Element {
     return (
         <div className = "sign-up-page">
             <div className = "sign-up-page--content">
+                <h1 className = "sign-up-page--form-header">Let's Make An Account! </h1>
                 <div className = "sign-up-page--form-container"> 
-                    <h1>Let's Make An Account! </h1>
                     <div className = "form-container--content">
                         <Form>
-                            {/* <Form.Group className="mb-3 col">
-                                    
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Gender</Form.Label> 
-                                <Form.Select className = "Form--Selection">
-                                    <option value = "Male"> Male </option>
-                                    <option value = "Female"> Female </option>
-                                    <option value = "N/A"> Prefer Not To Say</option>
-                                </Form.Select>
-                            </Form.Group> */}
+                            <Container fluid>
+                                <Row className="form--row-padding">
+                                    <Form.Label > Name </Form.Label>
+                                    <Col>
+                                        <Form.Group>
+                                            <Form.FloatingLabel label = "First Name">
+                                                <Form.Control placeholder="First Name">
+                                                </Form.Control>
+                                            </Form.FloatingLabel>
+                                        </Form.Group>
+                                    </Col>
+                                    <Col>
+                                        <Form.Group>
+                                            <Form.FloatingLabel label = "Last Name">
+                                                <Form.Control placeholder="Last Name">
+                                                </Form.Control>
+                                            </Form.FloatingLabel>
+                                        </Form.Group>
+                                    </Col>
+                                </Row>
+                                <Row className="form--row-padding">
+                                    <Form.Group>
+                                        <Form.Label>Gender</Form.Label> 
+                                        <Form.Select className = "Form--Selection">
+                                            <option value = "Male"> Male </option>
+                                            <option value = "Female"> Female </option>
+                                            <option value = "N/A"> Prefer Not To Say</option>
+                                        </Form.Select>
+                                    </Form.Group>
+                                </Row>
+                            </Container>
                         </Form>
                     </div>
                 </div>
