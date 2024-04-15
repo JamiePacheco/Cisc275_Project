ab130440fb6eef9126d6133b1e82b683011e1460
Claudia Chance <cchance@udel.edu>
2024-04-14 23:27:59 -0400

    added home button yay

--- src/Pages/ReportsPage/Components/ReportsHeader.tsx
+++ src/Pages/ReportsPage/Components/ReportsHeader.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom"
 
 export function ReportsHeader() : JSX.Element {
+    const nav = useNavigate();
     return(
     <div className='header-report-page'>
         <h1>Reports Page</h1>
         <p>Placeholder for Reports Body</p>
+        <button onClick = {() => nav("/home")}> Home </button>
     </div>
     )
-}
+};
