3464b6b6e4365dfacb9f1a3a091354889d399d91
Claudia Chance <cchance@udel.edu>
2024-04-24 08:32:11 -0400

    small changes, colors, etc

--- src/Pages/ReportsPage/ReportsPage.tsx
+++ src/Pages/ReportsPage/ReportsPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './ReportsPage'
-import { ReportsHeader } from './Components/ReportsHeader'
+import { ReportsHeader } from './Components/ReportsHeader/ReportsHeader'
+// import { FolderBackground } from "./Components/Folder/Folder-Background"
 
 export function ReportsPage() : JSX.Element{
     return(
