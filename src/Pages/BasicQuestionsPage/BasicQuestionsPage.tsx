b659907042a911e55aafd234cc7c1145429aca4f
Nathan Wood <zestyalgaewafer@gmail.com>
2024-04-24 20:09:36 -0400

    added actual questions, saved question data, added submit button

--- src/Pages/BasicQuestionsPage/BasicQuestionsPage.tsx
+++ src/Pages/BasicQuestionsPage/BasicQuestionsPage.tsx
@@ -1,18 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-
-import { useState } from 'react';
 import './BasicQuestionsPage.css';
 import { BasicQuestionsPageHeader } from "./Components/BasicQuestionsPageHeader/BasicQuestionsPageHeader";
 import { GeneralQuestions } from "./Components/GeneralQuestionWidget/GeneralQuestionWidget";
-import { QuizInteraction } from './Components/QuizInteraction/QuizInteraction';
-import { ProgressBarWidget } from './Components/ProgressBarWidget/ProgressBarWidget';
+import { RevealAnswers } from './Components/QuizInteraction/RevealAnswersButton/RevealAnswersButton';
+import { useState } from 'react';
+
 
 export function BasicQuestionsPage() : React.JSX.Element {
+    const [isSubmitted, setIsSubmitted] = useState(false);
 
     return (
         <div className='BasicQuestionsPage'> 
             <BasicQuestionsPageHeader/>
             <GeneralQuestions/>
+            <RevealAnswers/>
         </div>
     );
 }
--- src/Pages/BasicQuestionsPage/Components/QuizInteraction/QuizInteraction.tsx
+++ src/Pages/BasicQuestionsPage/Components/QuizInteraction/QuizInteraction.tsx
@@ -1,18 +1,23 @@
 import "./QuizInteraction.css";
 import { IndexState } from "./QuestionIndexProp";
+import { placeholders } from "../Interfaces/BasicQuestionInterface";
 
-export function QuizInteraction({ setIndex, index, restoreAnswer}: IndexState): JSX.Element {
+export function QuizInteraction({ setIndex, index, isProgressBarFull }: IndexState): JSX.Element {
   const handleClick = (value: boolean) => {
-    setIndex(value && index < 6 ? index + 1: !value && index > 0 ? index -1 : index );
-    restoreAnswer();
+    setIndex(value && index < 7 ? index + 1: !value && index > 0 ? index -1 : index );
   }
 
   return (
     <div className="buttons">
-      <button className="previous-button" onClick={() => handleClick(false)}>
+      <button className="previous-button" onClick={() => handleClick(false)} disabled={index === 0}>
         Previous
       </button>
-      <button className="next-button" onClick={() =>handleClick(true)}>
+      {isProgressBarFull && ( //temporarily set as a browser alert until fully implemented 
+        <button className="submit-button" onClick={() => alert('Answers Submitted')}> 
+          Submit
+        </button>
+      )}
+      <button className="next-button" onClick={() => handleClick(true)} disabled={index === placeholders.length - 1}>
         Next
       </button>
     </div>
