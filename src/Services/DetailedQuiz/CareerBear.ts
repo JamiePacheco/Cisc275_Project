5c5bfd41bbbb6dca65ab52ae7718b762eda3a59d
Jamie <jamie.pacheco.71@gmail.com>
2024-05-02 01:02:28 -0400

    added line suppressment

--- src/Services/DetailedQuiz/CareerBear.ts
+++ src/Services/DetailedQuiz/CareerBear.ts
@@ -220,7 +220,7 @@ export async function notifyUser() : Promise<OpenAI.Chat.Completions.ChatComplet
 }
 
 //I would never usually do this but for some reason the field 'response_format' is not valid on some machines but valid on others
-//@ts-ignore
+
 export async function evaluateUserCareerFromQuiz(quizData : DetailedQuiz) {
   let completion = null;
 
@@ -254,9 +254,8 @@ export async function evaluateUserCareerFromQuiz(quizData : DetailedQuiz) {
           content : "Format the user's personality traits and career suggestions into the json format specificed," + USER_DATA_FORMAT
         }
       ],
-      response_format : {
-        type : "json_object"
-      },
+      //@ts-ignore
+      response_format : {type : "json_object"},
       model : "gpt-4-turbo"
     });
 
