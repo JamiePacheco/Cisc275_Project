1158c90cd76a74ee435466eecb8f7b8c37585c45
Jamie <jamie.pacheco.71@gmail.com>
2024-05-03 01:55:51 -0400

    began to work on loading screen message

--- src/Components/LoadingScreen/LoadingScreen.tsx
+++ src/Components/LoadingScreen/LoadingScreen.tsx
@@ -1,12 +1,24 @@
 import BearChairRight from "../../assets/career-bear/bear-in-chair_right.png"
 import BearChairLeft from "../../assets/career-bear/bear-in-chair_left.jpg"
 import { useEffect, useState } from "react"
+import { useTypeWriter } from "../../Hooks/useTypeWriter"
 
 const images = [BearChairLeft, BearChairRight]
 
-export function LoadingScreen({message} : {message : string}) : React.JSX.Element {
+const funny_loading_quips = [
+    "'career bear is processing user data...'",
+    "'career bear is snacking on some grapes...'",
+    "'career bear is thinking about what's for dinner...'",
+    "'career bear is getting sleepy...'"
+]
+
+export function LoadingScreen() : React.JSX.Element {
     
     const [currentImage, setCurrentImage] = useState<number>(0);
+
+    const [loadingMessage, setLoadingMessage] = useState<number>(0)
+
+    const message = useTypeWriter(funny_loading_quips[loadingMessage])
 
     useEffect(() => {
         const intervalid = setInterval(() => {
@@ -16,6 +28,13 @@ export function LoadingScreen({message} : {message : string}) : React.JSX.Elemen
         return () => clearInterval(intervalid);
     }, [])
 
+    useEffect(() => {
+        const intervalid = setInterval(() => {
+            setLoadingMessage(prev => (prev + 1) % funny_loading_quips.length)
+        }, 5000)
+
+        return () => clearInterval(intervalid);
+    }, [])
 
     return (
         <div className = "loading-screen-body">
