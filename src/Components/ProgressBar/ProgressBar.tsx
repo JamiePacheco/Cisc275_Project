650cae6d4ab0b24faea68520dd0d4bd71b09b888
water-plant <arsconduit@gmail.com>
2024-05-14 09:55:25 -0400

    Add progress bar animations and Fix career bear positioning

--- src/Components/ProgressBar/ProgressBar.tsx
+++ src/Components/ProgressBar/ProgressBar.tsx
@@ -4,7 +4,7 @@ import { ProgressBar } from "react-bootstrap";
 import careerBearSleeping from "../../assets/career-bear/sleeping-career-bear.png"
 
 import careerBearIntern from "../../assets/career-bear/career-bear-2-neutral.png"
-import { useEffect, useState } from "react";
+import { useRef, useEffect, useState, ReactElement } from "react";
 
 export type Bear = "career" | "intern" 
 
@@ -16,10 +16,21 @@ export function CareerProgressBear({curr, total, mode} : {curr : number, total :
 
     const [integerProgression, setIntegerProgression] = useState(0);
 
+    const [width, setWidth] = useState<number>(0);
+
+    const element = useRef<HTMLDivElement>(null);
+
     useEffect(() => {
         setProgressConstant((curr / total) * 100);
         setIntegerProgression(curr === 1 ? 0 : curr);
     }, [curr, total])
+
+
+    useEffect( () => {
+        if(element.current){
+            setWidth(element.current.offsetWidth);
+        }
+    })
 
     useEffect(() => {
         
@@ -35,8 +46,10 @@ export function CareerProgressBear({curr, total, mode} : {curr : number, total :
     console.log((progressConstant)* 45)
     return (
         <div className="progress-bar-bootstrap">
-            <ProgressBar now={progressConstant} className="custom-progress-bar" color="#6c4c41"/>
-            <img src = {careerBearSleeping} alt = "" className = "progress-bear-image" style = {{left : `${(integerProgression) * 45 + (progressConstant/2)}px`}} />
+            <div ref={element} className="progress-bar--component">
+                <ProgressBar ref={element} now={progressConstant} className="custom-progress-bar" color="#6c4c41"/>
+            </div>
+            <img src = {careerBearSleeping} alt = "" className = "progress-bear-image" style = {{left :  `${ curr > 1 && curr <= total ? width* (curr-1)/total: curr <=1? 0: width}px`}} />
       </div>
-    )
+    )   
 }
