a54a04e0ab5129d9ce4cf315e41a674fc09d2474
Claudia Chance <cchance@udel.edu>
2024-05-15 19:58:54 -0400

    Writing and stuff for basic

--- src/Pages/ReportsPage/Components/BasicFolderBackground/CareerSuggestionView/CareerSuggestion.tsx
+++ src/Pages/ReportsPage/Components/BasicFolderBackground/CareerSuggestionView/CareerSuggestion.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import './CareerSuggestion.css'
-
 import { CareerFieldSuggestion } from '../../../../../Interfaces/Results/CareerFieldSuggestion'
 import { Col, Container, Row } from "react-bootstrap";
 
 export function CareerSuggestionView({data} : {data : CareerFieldSuggestion}) : React.JSX.Element {
+
     return (
         <div className = 'career-tab-containers'>
             <h3> {data.careerField} </h3>
@@ -19,14 +19,21 @@ export function CareerSuggestionView({data} : {data : CareerFieldSuggestion}) : 
                             <p>{data.careerFieldDescription}</p>
                         </div>
 
-                        <div>
-                            <h5>Suggested Jobs</h5>
-                            <p>{data.careerFieldJobs}</p>
+                        <div className = "careertab-paragraphstyle">
+                            <h5>How These Were Selected</h5>
+                            <p>
+                                {data.careerFieldLogic}
+                            </p>
                         </div>
 
                         </Col>
 
                         <Col>
+                            <div className = "careertab-paragraphstyle">
+                            <h5>Suggested Jobs</h5>
+                                <p>
+                                {data.careerFieldJobs.slice(0, -1).join(', ') + ', and ' + data.careerFieldJobs[data.careerFieldJobs.length - 1]}</p>
+                            </div>
                         </Col>
 
                     </Row>
