43e362c3deda0a48797442e7726f02998861e2f5
Jamie Pacheco <jamie.pacheco.71@gmail.com>
2024-05-16 07:41:46 -0400

    finished implementing results page for detailed quiz

--- src/Pages/ReportsPage/Components/BasicFolderBackground/PersonalityView/PersonalityView.tsx
+++ src/Pages/ReportsPage/Components/BasicFolderBackground/PersonalityView/PersonalityView.tsx
@@ -1,7 +1,7 @@
 import { PersonalityTrait } from '../../../../../Interfaces/Results/PersonalityTrait';
 import "./PersonalityView.css"
 // import BasicSticker from '../../../BasicCareerSticker/BasicCareerSticker'
-
+import slushiBoy from "../../../../../assets/career-intern/fitzwithslushie.png"
 
 export function PersonalityView({personalityData} : {personalityData : PersonalityTrait[] | undefined}) : JSX.Element {
     const mappedPersonalityData = personalityData === undefined ? "Data Not Found..." : personalityData.map((per : PersonalityTrait) => {
@@ -19,7 +19,12 @@ export function PersonalityView({personalityData} : {personalityData : Personali
                  <div className="personality-tab-container-content">
                     <div className = "personality-row">
                         {mappedPersonalityData}
+                        <div className = "personality-tab-content">
+                            <img src = {slushiBoy} alt = "slushi boy"/>
+                        </div>
                         <div className = 'career-bear-sticker-container-2'>
+
+
                             {/* <BasicSticker/> */}
                         </div>
                     </div>
