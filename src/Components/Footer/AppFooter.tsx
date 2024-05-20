68d05d80192aa28c9234d1ee54eab1efc13b00f7
Claudia Chance <cchance@udel.edu>
2024-05-19 21:23:12 -0400

    adding no PI disclaimer

--- src/Components/Footer/AppFooter.tsx
+++ src/Components/Footer/AppFooter.tsx
@@ -13,7 +13,11 @@ export function AppFooter() : React.JSX.Element {
               <li className="list-inline-item"><a href="#/about">About</a></li>
               <li className="list-inline-item"><a href="mailto:careerbearteam@gmail.com">Report Bugs</a></li>
             </ul>
-            <p className="copyright">CareerBear © 2024</p>
+            <p className = "disclaimer">
+                While it is tempting, do not share any personal information with CareerBear!
+            </p>
+            <p className="copyright">CareerBear © 2024</p> 
+
           </footer>
         </div>
       );
