3285a893e22a979bae178f4df3e5c11466743061
Claudia Chance <cchance@udel.edu>
2024-05-19 20:53:21 -0400

    Adding Footer stuffs

--- src/Components/Footer/AppFooter.tsx
+++ src/Components/Footer/AppFooter.tsx
@@ -1,13 +1,21 @@
+import React from 'react';
 
 import "./AppFooter.css"
 
 
 
 export function AppFooter() : React.JSX.Element {
-
-
     return (
-        <footer className = "App-footer"> 
-        </footer>
-    )
-}
+        <div className="App-footer">
+          <footer>
+            <ul className="list-inline">
+              <li className="list-inline-item"><a href="#/home">Home</a></li>
+              <li className="list-inline-item"><a href="#/about">About</a></li>
+              <li className="list-inline-item"><a href="mailto:careerbear@gmail.com">Report Bugs</a></li>
+            </ul>
+            <p className="copyright">CareerBear © 2024</p>
+          </footer>
+        </div>
+      );
+    };
+    
